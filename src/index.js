8e838671af358f634d65879a76cec30f5c7b7063
Garhaughawout <108907661+Garhaughawout@users.noreply.github.com>
2024-04-04 13:46:17 -0500

    Final version with all requirements
    completed the readme requirements

--- src/index.js
+++ src/index.js
@@ -16,3 +16,36 @@
 /* ----------------------------------------------------- 
     GET REQUEST
 ----------------------------------------------------- */
+
+fetch("http://localhost:3000/characters")
+    .then((resp) => resp.json())
+    .then((data) => renderCharArray(data))    
+
+function renderCharArray(data) {
+    data.forEach(character => {
+        (character) 
+            // selecting the Unordered List and then creating the necessary elements for displaying the information.
+            const characterList = document.querySelector("#characters")
+            const li = document.createElement("li")
+            const image = document.createElement("img")
+
+            image.src = character.image
+
+            li.innerHTML = `
+                <p>${character.name}</p>
+                <h3>${character.age}</h3>
+                ${image.outerHTML}
+                `
+            characterList.append(li)
+            
+            
+            // event listeners for the list items
+            li.addEventListener("mouseover", () => {
+                li.style.border = "1px solid black"
+            })
+            li.addEventListener("mouseout", () => {
+                li.style.border = "none"
+            })
+        }
+    )
+}
